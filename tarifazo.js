4ee802016a66e81920552fe779f01d4b7b69cde1
Juanmaanghilante <curne_juan@hotmail.com>
2023-04-18 09:13:00 -0300

    modificacion tarifazo

--- tarifazo.js
+++ tarifazo.js
@@ -6,3 +6,5 @@ let ajuste = consumKwh >=300 ? pagoMes += pagoMes* 0.20: 'sin ajuste tarifario'
 
 console.log( 'se ajusto su tarifa electrica en un 20% ahora su boleta es de   $' +ajuste);
 console.log(ajuste);
+console.log(ajuste);
+console.log(ajuste);
